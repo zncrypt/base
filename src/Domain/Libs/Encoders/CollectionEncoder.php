1247fb5d6d0ee7047688e2212e39cdf90990a789
Vitaliy <theyamshikov@yandex.ru>
2022-06-08 20:57:42 +0600

    upd

--- src/Domain/Libs/Encoders/CollectionEncoder.php
+++ src/Domain/Libs/Encoders/CollectionEncoder.php
@@ -3,6 +3,9 @@
 namespace ZnCrypt\Base\Domain\Libs\Encoders;
 
 use Illuminate\Support\Collection;
+use Psr\Container\ContainerInterface;
+use ZnCore\Base\Libs\Code\InstanceResolver;
+use ZnCore\Base\Libs\Container\Traits\ContainerAwareTrait;
 use ZnCrypt\Base\Domain\Libs\Encoders\EncoderInterface;
 use ZnCore\Domain\Helpers\EntityHelper;
 use ZnCore\Base\Helpers\InstanceHelper;
@@ -10,11 +13,14 @@ use ZnCore\Base\Helpers\InstanceHelper;
 class CollectionEncoder implements EncoderInterface
 {
 
+    use ContainerAwareTrait;
+
     private $encoderCollection;
 
-    public function __construct(Collection $encoderCollection)
+    public function __construct(Collection $encoderCollection, ContainerInterface $container = null)
     {
         $this->encoderCollection = $encoderCollection;
+        $this->setContainer($container);
     }
 
     public function encode($data) {
@@ -22,6 +28,7 @@ class CollectionEncoder implements EncoderInterface
         $encoders = $this->encoderCollection->all();
         foreach ($encoders as $encoderClass) {
             /** @var EncoderInterface $encoderInstance */
+//            (new InstanceResolver($this->ensureContainer()))->ensure();
             $encoderInstance = InstanceHelper::ensure($encoderClass);
             $data = $encoderInstance->encode($data);
         }
